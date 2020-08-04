0c974a1ccda7867bcd3fc039f48c50f01368784b
khanamoto <khanamoto10@gmail.com>
2020-08-04 23:33:01 +0900

    Update validation

--- model/sub_membership.go
+++ model/sub_membership.go
@@ -4,5 +4,5 @@ type SubMembership struct {
 	ID              uint64 `db:"id"`
 	UserID          uint64 `db:"user_id"`
 	SubStudyGroupID uint64 `db:"sub_study_group_id"`
-	Authority       int32  `db:"authority"`
+	Authority       int32  `db:"authority" validate:"required,oneof=1 2"`
 }
--- model/sub_study_group.go
+++ model/sub_study_group.go
@@ -3,6 +3,6 @@ package model
 type SubStudyGroup struct {
 	ID           uint64 `db:"id"`
 	StudyGroupID uint64 `db:"study_group_id"`
-	Name         string `db:"name"`
-	URL          string `db:"url"`
+	Name         string `db:"name" validate:"required,max=255"`
+	URL          string `db:"url" validate:"required,alphanum,max=255"`
 }
